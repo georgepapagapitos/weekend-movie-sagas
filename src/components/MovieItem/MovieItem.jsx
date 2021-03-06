1e8b570b0c0ef333c38a97d84fc7c180b1a1fcfb
George Papagapitos <george.g.papagapitos@gmail.com>
2021-03-06 10:51:17 -0600

    image_click dispatch is logging

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,8 +1,21 @@
+import { useDispatch } from 'react-redux';
+
 function MovieItem({ movie }) {
+
+  const dispatch = useDispatch();
+
+  const handleClick = (event) => {
+    console.log('clicked!', event.target);
+    dispatch({
+      type: 'IMAGE_CLICK',
+      payload: movie.id
+    });
+  }
+
   return (
     <div>
       <h3>{movie.title}</h3>
-      <img src={movie.poster} alt={movie.title}/>
+      <img src={movie.poster} alt={movie.title} onClick={handleClick}/>
     </div>
   )
 }
